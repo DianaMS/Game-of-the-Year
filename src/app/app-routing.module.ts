ba17d56bdea38cde43e4e17f786d8775d53a5e9f
DianaMS <dmartinezs@uni.pe>
2020-09-19 12:11:16 -0500

    route configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { GotyComponent } from './pages/goty/goty.component';
+
+
+const routes: Routes = [
+{ path: 'inicio', component: InicioComponent },
+{ path: 'goty', component: GotyComponent },
+{ path: '**', pathMatch: 'full', redirectTo: 'inicio'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
