9273b128d6f385a7b46cd574a0dc79e9131a71bb
DianaMS <dmartinezs@uni.pe>
2020-09-20 18:09:32 -0500

    configuration firebase production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
   url: 'https://us-central1-firestore-grafica-ce6c6.cloudfunctions.net',
+  firebase: {
+    apiKey: 'AIzaSyCUXLpLENPrlp-ORKTdk9I0frRN-jm-h1I',
+    authDomain: 'firestore-grafica-ce6c6.firebaseapp.com',
+    databaseURL: 'https://firestore-grafica-ce6c6.firebaseio.com',
+    projectId: 'firestore-grafica-ce6c6',
+    storageBucket: 'firestore-grafica-ce6c6.appspot.com',
+    messagingSenderId: '155537951761',
+    appId: '1:155537951761:web:cbc8fccc2b487e9c8bf5b0'
+  }
 };
