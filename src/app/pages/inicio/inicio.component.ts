51c588c35a9706511f8da4addf6fda54a30d76d7
DianaMS <dmartinezs@uni.pe>
2020-09-20 18:13:09 -0500

    get data from goty collection

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+import { Game } from '../../interfaces/game.interface';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  constructor( private db: AngularFirestore ) { }
 
   ngOnInit(): void {
+    this.db.collection('goty').valueChanges()
+      .pipe(
+        map((resp: Game[]) => {
+          return resp.map(({name, votos}) => ({name, value: votos}));
+        })
+      )
+      .subscribe( resp => {
+        console.log(resp)
+      })
   }
 
 }
