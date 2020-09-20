41c4a0c9b617d21c6a5daea48e7b3f4f459e3f22
DianaMS <dmartinezs@uni.pe>
2020-09-20 18:09:02 -0500

    local firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,15 @@
 export const environment = {
   production: false,
   url: 'http://localhost:5000/firestore-grafica-ce6c6/us-central1',
+  firebase: {
+    apiKey: 'AIzaSyCUXLpLENPrlp-ORKTdk9I0frRN-jm-h1I',
+    authDomain: 'firestore-grafica-ce6c6.firebaseapp.com',
+    databaseURL: 'https://firestore-grafica-ce6c6.firebaseio.com',
+    projectId: 'firestore-grafica-ce6c6',
+    storageBucket: 'firestore-grafica-ce6c6.appspot.com',
+    messagingSenderId: '155537951761',
+    appId: '1:155537951761:web:cbc8fccc2b487e9c8bf5b0'
+  }
 };
 
 /*
